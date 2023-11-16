b1f757dff2d0fc6de78f84db8709dc9deb98c12f
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-11-16 16:49:03 -0500

    feat: adjust matching API

--- src/graph.rs
+++ src/graph.rs
@@ -1,6 +1,4 @@
 use crate::dinic::DinicGraph;
-use rand::prelude::SliceRandom;
-use rand::Rng;
 use serde::Deserialize;
 use std::{fs::File, path::Path};
 
@@ -10,18 +8,13 @@ pub struct Graph {
     edges: Box<[Box<[usize]>]>,
 }
 
-struct ShuffledDinicGraph {
-    graph: DinicGraph,
-    left: Box<[usize]>,
-    right: Box<[usize]>,
+pub struct Match {
+    edges: Box<[(usize, usize)]>,
 }
 
-impl ShuffledDinicGraph {
-    pub fn current_flow(&self) -> impl Iterator<Item = (usize, usize)> + '_ {
-        self.graph
-            .current_flow()
-            .filter(|x| x.0 < self.left.len())
-            .map(|(u, v, _)| (self.left[u], self.right[v - self.right.len()]))
+impl Match {
+    pub fn size(&self) -> usize {
+        self.edges.len()
     }
 }
 
@@ -30,7 +23,7 @@ impl Graph {
         let file = File::open(x)?;
         simd_json::from_reader(file).map_err(Into::into)
     }
-    pub fn as_shuffled_dinic_graph<G: Rng>(&self, rng: &mut G) -> ShuffledDinicGraph {
+    pub fn find_match(&self) -> Match {
         let src = 2 * self.size;
         let sink = 2 * self.size + 1;
         let mut graph = DinicGraph::new(2 * self.size + 2, src, sink);
@@ -38,27 +31,18 @@ impl Graph {
             graph.add_edge(src, i, 1);
             graph.add_edge(i + self.size, sink, 1);
         }
-        let mut left_shuffle = (0..self.size).collect::<Box<[usize]>>();
-        let mut right_shuffle = (0..self.size).collect::<Box<[usize]>>();
-        left_shuffle.shuffle(rng);
-        right_shuffle.shuffle(rng);
         for (u, edges) in self.edges.iter().enumerate() {
             for v in edges.iter().copied() {
-                graph.add_edge(left_shuffle[u], right_shuffle[v] + self.size, 1);
+                graph.add_edge(u, v + self.size, 1);
             }
         }
-        let mut left_decode = vec![0; self.size].into_boxed_slice();
-        let mut right_decode = vec![0; self.size].into_boxed_slice();
-        for (i, &x) in left_shuffle.iter().enumerate() {
-            left_decode[x] = i;
+        let flow = graph.calculate_flow();
+        let mut edges = Vec::with_capacity(flow as usize);
+        for (u, v, _) in graph.current_flow().filter(|x|x.0 < self.size) {
+            edges.push((u, v - self.size));
         }
-        for (i, &x) in right_shuffle.iter().enumerate() {
-            right_decode[x] = i;
-        }
-        ShuffledDinicGraph {
-            graph,
-            left: left_decode,
-            right: right_decode,
+        Match {
+            edges: edges.into_boxed_slice(),
         }
     }
 }
@@ -73,11 +57,9 @@ mod test {
         let path = path.join("data").join("box.json");
         let graph = super::Graph::load(path).unwrap();
         println!("{:?}", graph);
-        let mut dinic = graph.as_shuffled_dinic_graph(&mut rand::thread_rng());
-        let flow = dinic.graph.calculate_flow();
-        println!("flow: {}", flow);
-        assert_eq!(flow as usize, graph.size);
-        for (u, v) in dinic.current_flow() {
+        let matching = graph.find_match();
+        assert_eq!(matching.size(), graph.size);
+        for (u, v) in matching.edges.iter() {
             println!("{} -> {}", u, v);
             assert_eq!(u, v);
         }
@@ -89,11 +71,9 @@ mod test {
         let path = path.join("data").join("complete.json");
         let graph = super::Graph::load(path).unwrap();
         println!("{:?}", graph);
-        let mut dinic = graph.as_shuffled_dinic_graph(&mut rand::thread_rng());
-        let flow = dinic.graph.calculate_flow();
-        println!("flow: {}", flow);
-        assert_eq!(flow as usize, graph.size);
-        for (u, v) in dinic.current_flow() {
+        let matching = graph.find_match();
+        assert_eq!(matching.size(), graph.size);
+        for (u, v) in matching.edges.iter() {
             println!("{} -> {}", u, v);
         }
     }
